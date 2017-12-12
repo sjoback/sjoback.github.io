1d1cf9614f9b9bdc2ccdc40a6748918f8df31b91
Joakim Sjöbäck <31646325+sjoback@users.noreply.github.com>
2017-12-12 15:33:25 +0100

    Add files via upload

---
+++ storytelling/js/bubble.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.small').fadeOut(1);
+    $('.small').removeClass('hidden');
+    $(".small").delay(1000).fadeIn(2000);
+});
+$(document).ready(function() {
+    $('.big').fadeOut(1);
+    $('.big').removeClass('hidden');
+   //  $('.big').fadeIn(1500);
+    $(".big").delay(2000).fadeIn(2000);
+});
+$(document).ready(function() {
+    $('.test').fadeOut(1);
+    $('.test').removeClass('hidden');
+    $(".test").delay(3000).fadeIn(2000);
+});
